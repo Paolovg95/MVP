e271c5f40907ced827b0c9347a410c9b4d8d590d
Paolo <paolo9517@gmail.com>
2023-10-28 19:38:11 -0300

    Return render using template and dictionary

--- emails/views.py
+++ emails/views.py
@@ -3,11 +3,13 @@ from .models import EmailEntry
 from django.http import HttpResponse, Http404
 # Create your views here.
 
+#html_str = "<!doctype html><html><body><h1>{email}</h1></body></html>"
+
 def email_entry_get_view(request, id=id, *args, **kwargs):
     print(args,kwargs)
     try:
         obj = EmailEntry.objects.get(id=id)
+        #my_html = html_str.format(email=obj.email)
     except EmailEntry.DoesNotExist:
         raise Http404
-
-    return HttpResponse(f"<h1> Hello {obj.email}</h1>")
+    return render(request, "get.html", {"obj": obj})
