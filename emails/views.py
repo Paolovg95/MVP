cdfd4dace36dbe57a8a9f22e7f41f9bbcac472fe
Paolo <paolo9517@gmail.com>
2023-10-28 16:46:10 -0300

    Function Based View added to display

--- emails/views.py
+++ emails/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import EmailEntry
+from django.http import HttpResponse, Http404
 # Create your views here.
+
+def email_entry_get_view(request, id=id, *args, **kwargs):
+    print(args,kwargs)
+    try:
+        obj = EmailEntry.objects.get(id=id)
+    except EmailEntry.DoesNotExist:
+        raise Http404
+
+    return HttpResponse(f"<h1> Hello {obj.email}</h1>")
