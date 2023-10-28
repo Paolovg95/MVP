4d1b54cbbdba630072d4b1d16a015fa798983420
Paolo <paolo9517@gmail.com>
2023-10-28 15:00:50 -0300

    Name field added to EmailEntry Model

--- emails/models.py
+++ emails/models.py
@@ -4,5 +4,6 @@ from django.db import models
 
 class EmailEntry(models.Model):
     email = models.EmailField()
+    name = models.CharField(max_length=100)
     updated_at = models.DateTimeField(auto_now=True) # Date time updated
     timestamp = models.DateTimeField(auto_now_add=True) # Date time added
