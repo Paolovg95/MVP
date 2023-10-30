d7df852bfb5948ba1ec8b231340c9083ce87ab88
Paolo <paolo9517@gmail.com>
2023-10-30 18:32:42 -0300

    Create View and saving Form

--- emails/views.py
+++ emails/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import EmailEntry
 from django.http import HttpResponse, Http404
+from .forms import EmailEntryForm
+
 # Create your views here.
 
 #html_str = "<!doctype html><html><body><h1>{email}</h1></body></html>"
@@ -13,3 +15,19 @@ def email_entry_get_view(request, id=id, *args, **kwargs):
     except EmailEntry.DoesNotExist:
         raise Http404
     return render(request, "get.html", {"obj": obj})
+
+
+def email_entry_create_view(request, *args, **kwargs):
+    form = EmailEntryForm(request.POST)
+    if request.method == 'POST':
+        #print(request.POST)
+        if form.is_valid():
+            # obj = form.save(commit=False)
+            # obj.name = "N/A"
+            # obj.save()
+            #print(form.clean_email())
+            form.save()
+
+            form = EmailEntryForm()
+
+    return render(request, "form.html", {'form': form})
